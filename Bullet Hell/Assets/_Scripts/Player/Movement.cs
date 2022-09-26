39e51e7f1a9f05174732cd0ca06e963bcbd6e466
Carl Christensen <carl.christensen98@gmail.com>
2022-09-26 22:41:01 +0200

    Added simple player movement
    I made an Action map using the new Input System. Then I bound the performed and cancel-actions to regulate the player object's velocity. Doing it this way, I don't have to use any update methods for movement.

---
+++ Bullet Hell/Assets/_Scripts/Player/Movement.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private Rigidbody2D rigidBody;
+    private Vector2 movementVector;
+    private float playerSpeed = 5;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerInput = new PlayerInput();
+        playerInput.Enable();
+        playerInput.Actions.Move.performed += NormalizeMovement;
+        playerInput.Actions.Move.canceled += NormalizeMovement;
+    }
+
+    private void NormalizeMovement(InputAction.CallbackContext context)
+    {
+        movementVector = context.ReadValue<Vector2>();
+
+        if(movementVector.magnitude <= 1) MovePlayer(movementVector);
+
+        else MovePlayer(movementVector.normalized);
+    }
+    private void MovePlayer(Vector2 playerInput)
+    {
+        rigidBody.velocity = playerInput * playerSpeed;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        MovePlayer(movementVector);
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Actions.Move.performed -= NormalizeMovement;
+        playerInput.Actions.Move.canceled -= NormalizeMovement;
+        playerInput.Disable();
+    }
+}
