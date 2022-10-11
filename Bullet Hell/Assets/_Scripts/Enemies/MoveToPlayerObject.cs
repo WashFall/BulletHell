3f34d2e5d2dd01e8b65c640343f5810e1a321daf
Carl Christensen <carl.christensen98@gmail.com>
2022-10-11 13:36:52 +0200

    Fixed enemy retaining velocity on collision

--- Bullet Hell/Assets/_Scripts/Enemies/MoveToPlayerObject.cs
+++ Bullet Hell/Assets/_Scripts/Enemies/MoveToPlayerObject.cs
@@ -8,10 +8,22 @@ public class MoveToPlayerObject : MonoBehaviour
     public GameObject playerObject;
 
     private float speed = 3;
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate()
     {
         transform.position = Vector2.MoveTowards
             (transform.position, playerObject.transform.position, Time.fixedDeltaTime * speed);
+        print(rigidBody.velocity);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        rigidBody.velocity = Vector2.zero;
     }
 }
