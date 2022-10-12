f404791d67488069393c4d9108b8de1d7d26af09
Carl Christensen <carl.christensen98@gmail.com>
2022-10-12 11:57:43 +0200

    I made an enemy spawner that spawns enemies off the screen

---
+++ Bullet Hell/Assets/_Scripts/Enemies/SpawnEnemies.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject player;
+    private bool canSpawn = true;
+    private float spawnRate = 1;
+    private float cameraWidth;
+
+    private void Awake()
+    {
+        cameraWidth = Camera.main.aspect * (Camera.main.orthographicSize) + 1;
+    }
+
+    void Update()
+    {
+        if (canSpawn)
+        {
+            canSpawn = false;
+            StartCoroutine(nameof(spawnEnemy));
+        }
+    }
+
+    private IEnumerator spawnEnemy()
+    {
+        yield return new WaitForSeconds(spawnRate);
+
+        Vector2 enemySpawnPoint = (Vector2)transform.position + (Random.insideUnitCircle.normalized * cameraWidth);
+        GameObject enemy = Instantiate(enemyPrefab, enemySpawnPoint, Quaternion.identity);
+        enemy.GetComponent<MoveToPlayerObject>().playerObject = player;
+        canSpawn = true;
+    }
+}
